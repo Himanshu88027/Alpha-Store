99dceb6bf30a17868c4ab74b2e5324cbba695ab5
Himanshu88027 <himanshukumar88027@gmail.com>
2025-05-11 13:03:33 +0530

    home-layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,6 +14,15 @@ export default function Home() {
         </div>
         <div>
           <Input placeholder="I am input"></Input>
+        </div>
+        <div>
+          <Progress value={60}></Progress>
+        </div>
+        <div>
+          <Textarea placeholder="I am textarea"></Textarea>
+        </div>
+        <div>
+          <Checkbox></Checkbox>
         </div>
       </div>
     </div>
